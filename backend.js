539b36be639ef0b69203d8ffd5024d14b19a59d4
Rishi Patel <rishi_1607@icloud.com>
2026-01-24 10:52:59 -0600

    Created function to create 16x16 number of grids.

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 let container = document.querySelector("#container");
 
 function createGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16*16; i++) {
         const grid = document.createElement("div");
         grid.setAttribute("id", `grid${i + 1}`);
         grid.appendChild(grid);
