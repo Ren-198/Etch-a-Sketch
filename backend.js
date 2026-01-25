40b02728507f56a40e211a0ed0614115b63323f1
Rishi Patel <rishi_1607@icloud.com>
2026-01-25 10:27:50 -0600

    Implements function to change grid to black.

--- backend.js
+++ backend.js
@@ -1,14 +1,23 @@
 let container = document.querySelector("#container");
+const gridSize = 16;
+totalCells = 16*16;
+
+function changeColor(event) {
+    event.target.style.backgroundColor = 'black';
+}
 
 function createGrid() {
-    for (let i = 0; i < 16*16; i++) {
+    for (let i = 0; i < totalCells; i++) {
         const grid = document.createElement("div");
-        grid.setAttribute("id", `grid${i + 1}`);
-        grid.appendChild(grid);
+        grid.setAttribute("class", "grid-item");
+        grid.addEventListener("mouseover", changeColor);
+        container.appendChild(grid);
 
     }
 }
 
 createGrid();
+
+
 
 
