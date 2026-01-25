6e000e0b1a3efeaf9e951403b5e19f55673f2d56
Rishi Patel <rishi_1607@icloud.com>
2026-01-25 10:53:28 -0600

    Completion of Etch-a-Sketch project.

--- backend.js
+++ backend.js
@@ -1,22 +1,47 @@
 let container = document.querySelector("#container");
+const setGrid = document.querySelector("button");
 const gridSize = 16;
+const containerSize = 950;
 totalCells = 16*16;
 
 function changeColor(event) {
     event.target.style.backgroundColor = 'black';
 }
 
-function createGrid() {
+function createGrid(gridSize) {
+    container.innerHTML = "";
+
+    totalCells = gridSize * gridSize;
+    const squareSize = containerSize / gridSize;
+
     for (let i = 0; i < totalCells; i++) {
         const grid = document.createElement("div");
         grid.setAttribute("class", "grid-item");
+        grid.style.width = `${squareSize}px`;
+        grid.style.height = `${squareSize}px`;
+
         grid.addEventListener("mouseover", changeColor);
         container.appendChild(grid);
 
     }
 }
 
-createGrid();
+setGrid.addEventListener('click', (event) => {
+    let input = prompt("Enter the number of squares per side: ");
+    let size = parseInt(input);
+
+    if (!size || size < 1 || size > 100) {
+        alert("Invalid input. please enter a number between 1 and 100. ")
+
+        return;
+    }
+
+    createGrid(size);
+    
+});
+
+createGrid(gridSize);
+
 
 
 
